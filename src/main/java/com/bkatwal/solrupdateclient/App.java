f5e7092795fa8bd521cc39efb8829f8565d44447
b0k006w <Walmart1234>
2018-10-15 10:18:17 +0530

    first

---
+++ src/main/java/com/bkatwal/solrupdateclient/App.java
@@ -0,0 +1,13 @@
+package com.bkatwal.solrupdateclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
